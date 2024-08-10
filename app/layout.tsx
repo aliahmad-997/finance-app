c5842c68c71a8d9cdd624c373ff095d0dd2b3cbc
Ali Ahmad <aliahmad_997@outlook.com>
2024-08-10 22:09:28 +0500

    feat: adding create new account

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
 import { ClerkProvider } from "@clerk/nextjs";
+
+import { Toaster } from "@/components/ui/sonner";
 import { QueryProvider } from "@/providers/query-provider";
+import { SheetProvider } from "@/providers/sheet-provider";
 
 import "./globals.css";
 
@@ -22,6 +24,8 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <SheetProvider />
+          <Toaster />
           <QueryProvider>{children}</QueryProvider>
         </body>
       </html>
